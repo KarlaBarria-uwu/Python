f4695e353506cde76ebe114b76b25d9a8a202ace
KarlaBarria-uwu <karl.barria@duocuc.cl>
2025-04-17 23:01:58 -0400

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,21 @@
+#Strings utilizado para almacenar textos.
+mensaje = "Hola, mundo"
+nombre = 'César'
+
+#CONCATENACIÓN: Unir dos o varios strings en cadena con el símbolo +
+saludo = "Hola"
+nombre ="Pedro"
+apellido "Pascal"
+print(saludo+""+nombre+""+apellido)
+
+#REPETICIÓN: Repetir string con el símbolo *
+print("Hola Jimmyto" * 3)
+
+#INDEXACIÓN: Accede a un carácter por su índice.
+palabra = "Antonia"
+print(palabra[0]) #accede a la primera letra
+print(palabra[-2]) #accede a la penúltima letra
+
+#SUBSTRINGS: Obtiene una parte del string indicando el inicio y terminoe captura
+texto = "Llanos Juan"
+print(texto[7:11]) #obtiene la palabra Juan
